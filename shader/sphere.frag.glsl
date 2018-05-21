08505f72ddb3931a08dc84b764a85fda2e5191c0
Matthew Crankshaw <mhcrankshaw2@gmail.com>
2018-05-22 09:53:41 +1200

    lighting working correctly(incorrect  angle)

--- shader/sphere.frag.glsl
+++ shader/sphere.frag.glsl
@@ -13,17 +13,21 @@ uniform sampler2D u_texture_Map;
 // Output from Fragment Shader
 out vec4 pixel_Colour;
 
-uniform vec4 Ia = vec4(0.3f, 0.3f, 0.3f, 1.0f);
-uniform vec4 Id = vec4(1.0f, 1.0f, 1.0f, 1.0f);
-uniform vec4 Is = vec4(1.0f, 1.0f, 1.0f, 1.0f);
+uniform vec4 Ia = vec4(0.05f, 0.05f, 0.05f, 1.0f);
+uniform vec4 Id = vec4(0.5f, 0.5f, 0.5f, 1.0f);
+uniform vec4 Is = vec4(0.1f, 0.1f, 0.1f, 1.0f);
 
-uniform vec4 Ka = vec4(0.25,     0.20725,  0.20725,  1.0);
-uniform vec4 Kd = vec4(1.0,      0.829,    0.82900,  1.0);
-uniform vec4 Ks = vec4(0.296648, 0.296648, 0.296648, 1.0);
+vec4 Ka = vec4(0.25,     0.20725,  0.20725,  1.0);
+vec4 Kd = vec4(1.0,      0.829,    0.82900,  1.0);
+vec4 Ks = vec4(0.296648, 0.296648, 0.296648, 1.0);
 uniform float a = 11.264;
 
 
 void main () {
+
+	Ka = texture(u_texture_Map, frag_UV.xy);
+	Kd = texture(u_texture_Map, frag_UV.xy);
+	Ks = texture(u_texture_Map, frag_UV.xy);
 
 	// Direction to Light (normalised)
 	vec4 l = normalize(-frag_Light_Direction);
@@ -51,5 +55,5 @@ void main () {
 	//----------------------------------------------
 	// Fragment Colour
 	//----------------------------------------------
-	pixel_Colour = Ta + Td + Ts + texture(u_texture_Map, frag_UV.xy);
+	pixel_Colour = Ta + Td + Ts;
 }
